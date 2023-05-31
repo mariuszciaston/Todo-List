e85c8a027803622bff5a65b021b0dc6a56051c2b
Mariusz <casmar90@gmail.com>
2023-05-31 12:58:53 +0200

    Show/hide nav on window resize

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const hamburger = document.querySelector('#hamburger');
 const nav = document.querySelector('.nav');
 
 hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('change');
 	nav.classList.toggle('sidebar-toggle');
+	hamburger.classList.toggle('change');
 });
+
+function showSidebar() {
+	if (window.matchMedia('(min-width: 768px)').matches) {
+		hamburger.classList.add('change');
+		nav.classList.remove('sidebar-toggle');
+	} else {
+		hamburger.classList.remove('change');
+		nav.classList.add('sidebar-toggle');
+	}
+}
+
+window.addEventListener('resize', () => {
+	showSidebar();
+});
+
+showSidebar();
