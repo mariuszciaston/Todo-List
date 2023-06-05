430c0f1987bc69998d17aba8dbd6e4683e7fd866
Mariusz <casmar90@gmail.com>
2023-06-05 17:41:09 +0200

    Add task, transitions, transparency mask on tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,46 @@
+const logicController = (() => {})();
 
-// Hamburger menu 
+const displayController = (() => {})();
+
+const addTaskBtn = document.querySelector('#add-task-btn');
+addTaskBtn.addEventListener('click', () => {
+	// const inputTask = prompt('Please enter task content', '');
+
+	const addTask = (() => {
+		const tasksList = document.querySelector('.tasks-list');
+		const task = document.createElement('li');
+		tasksList.appendChild(task);
+
+		const circle = document.createElement('div');
+		circle.className = 'circle';
+		task.appendChild(circle);
+
+		const checkMark = document.createElement('div');
+		checkMark.className = 'checkMark';
+		circle.appendChild(checkMark);
+
+		const taskContent = document.createElement('p');
+		taskContent.className = 'task-content';
+		// taskContent.textContent = inputTask;
+		taskContent.textContent = 'Example task';
+		task.appendChild(taskContent);
+
+		const date = document.createElement('p');
+		date.className = 'date';
+		date.textContent = 'no date set';
+		task.appendChild(date);
+
+		const star = document.createElement('div');
+		star.className = 'star';
+		task.appendChild(star);
+
+		const remove = document.createElement('div');
+		remove.className = 'remove';
+		task.appendChild(remove);
+	})();
+});
+
+// Hamburger menu
 const hamburger = document.querySelector('#hamburger');
 const main = document.querySelector('.main');
 
