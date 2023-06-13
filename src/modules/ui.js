91844d0df8c8e3e330602aa2f624599d5f2ed999
Mariusz <casmar90@gmail.com>
2023-06-13 17:16:04 +0200

    Add hamburgerMenuControl module

---
+++ src/modules/ui.js
@@ -0,0 +1,28 @@
+const hamburgerMenuControl = (() => {
+	const hamburger = document.querySelector('#hamburger');
+	const main = document.querySelector('.main');
+
+	const manualToggle = () => {
+		main.classList.toggle('sidebar-toggle');
+		hamburger.classList.toggle('change');
+	};
+
+	const autoToggle = () => {
+		if (window.matchMedia('(min-width: 800px)').matches) {
+			main.classList.remove('sidebar-toggle');
+			hamburger.classList.add('change');
+		} else {
+			main.classList.add('sidebar-toggle');
+			hamburger.classList.remove('change');
+		}
+	};
+	autoToggle();
+
+	hamburger.addEventListener('click', manualToggle);
+	window.addEventListener('resize', autoToggle);
+})();
+
+export default hamburgerMenuControl;
+
+
+// displayTasks
