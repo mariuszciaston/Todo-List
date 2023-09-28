5777f62f9944b956d28290a797a7df7fb520477d
Mariusz <casmar90@gmail.com>
2023-09-28 14:35:38 +0200

    Add ContentExample class

---
+++ src/modules/content.js
@@ -0,0 +1,83 @@
+export default class ContentExample {
+	static lists = ['Shopping', 'Movies to watch', 'Places to visit', 'Great ideas!'];
+
+	static tasks = [
+		'Bake Neapolitan pizza',
+		'Go swimming on Tuesday',
+		'Get hired as a Front End Developer',
+		'Conquer the Crown of Polish Mountains',
+		'Finish The Odin Project',
+	];
+
+	static shopping = [
+		'Apples',
+		'Bananas',
+		'Strawberries',
+		'Avocados',
+		'Bell Peppers',
+		'Carrots',
+		'Broccoli',
+		'Garlic',
+		'Lemons/Limes',
+		'Onion',
+		'Basil',
+		'Potatoes',
+		'Spinach',
+		'Tomatoes',
+	];
+
+	static placesToVisit = ['Amsterdam', 'Berlin', 'Madrid', 'Rome', 'London', 'Paris', 'Prague', 'Stockholm', 'Vienna'];
+
+	static moviesToWatch = [
+		'2001: A Space Odyssey (1968)',
+		'Blade Runner (1982)',
+		'Star Wars: Episode IV - A New Hope (1977)',
+		'Alien (1979)',
+		'Star Wars: Episode V - The Empire Strikes Back (1980)',
+		'Planet of the Apes (1968)',
+		'Star Trek II: The Wrath of Khan (1982)',
+		'The Matrix (1999)',
+		'The Thing (1982)',
+		'Jurassic Park (1993)',
+		'Aliens (1986)',
+		'E.T. the Extra-Terrestrial (1982)',
+		'A Clockwork Orange (1971)',
+		'The Day the Earth Stood Still (1951)',
+		'Invasion of the Body Snatchers (1956)',
+		'Metropolis (1927)',
+		'Terminator 2: Judgement Day (1991)',
+		'Forbidden Planet (1956)',
+		'Close Encounters of the Third Kind (1977)',
+		'Back to the Future (1985)',
+		'Brazil (1985)',
+		'Starship Troopers (1997)',
+		'Ex Machina (2014)',
+		'Wall-E (2008)',
+		'Inception (2010)',
+	];
+
+	static greatIdeas = [
+		'Travel to a country where you don’t speak the language',
+		'Go on a solo trip',
+		'Visit a “Dark Sky” site',
+		'Live abroad for a year',
+		'Take a pottery class',
+		'Plant a vegetable garden',
+		'Start a book club',
+		'Write a book',
+		'Teach a class',
+		'Become a mentor',
+		'Climb a mountain',
+		'Run a marathon',
+		'Start your own business',
+		'Plant a tree and watch it grow',
+		'Become a volunteer',
+		'Adopt a pet',
+		'Try skydiving',
+		'Take a hot air balloon ride',
+		'Learn to meditate',
+		'Join a dance class',
+		'Write a Letter to Your Future Self',
+		'Do Something, anything! You’ve never done',
+	];
+}
