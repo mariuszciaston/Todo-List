7d14f2479dd9d19f1a22200c3faeda796100f4a1
Mariusz <casmar90@gmail.com>
2023-09-28 14:37:11 +0200

    Move example content out of UI

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -2,6 +2,7 @@ import { format, parseISO, parse, isThisWeek, isToday, addDays } from 'date-fns'
 
 import Storage from './storage';
 import Sound from './sound';
+import ContentExample from './content';
 
 export default class UI {
 	static lastWidth = window.innerWidth;
@@ -754,88 +755,6 @@ export default class UI {
 	};
 
 	static loadExampleContent = () => {
-		const lists = ['Shopping', 'Movies to watch', 'Places to visit', 'Great ideas!'];
-
-		const tasks = [
-			'Bake Neapolitan pizza',
-			'Go swimming on Tuesday',
-			'Get hired as a Front End Developer',
-			'Conquer the Crown of Polish Mountains',
-			'Finish The Odin Project',
-		];
-
-		const shopping = [
-			'Apples',
-			'Bananas',
-			'Strawberries',
-			'Avocados',
-			'Bell Peppers',
-			'Carrots',
-			'Broccoli',
-			'Garlic',
-			'Lemons/Limes',
-			'Onion',
-			'Basil',
-			'Potatoes',
-			'Spinach',
-			'Tomatoes',
-		];
-
-		const placesToVisit = ['Amsterdam', 'Berlin', 'Madrid', 'Rome', 'London', 'Paris', 'Prague', 'Stockholm', 'Vienna'];
-
-		const moviesToWatch = [
-			'2001: A Space Odyssey (1968)',
-			'Blade Runner (1982)',
-			'Star Wars: Episode IV - A New Hope (1977)',
-			'Alien (1979)',
-			'Star Wars: Episode V - The Empire Strikes Back (1980)',
-			'Planet of the Apes (1968)',
-			'Star Trek II: The Wrath of Khan (1982)',
-			'The Matrix (1999)',
-			'The Thing (1982)',
-			'Jurassic Park (1993)',
-			'Aliens (1986)',
-			'E.T. the Extra-Terrestrial (1982)',
-			'A Clockwork Orange (1971)',
-			'The Day the Earth Stood Still (1951)',
-			'Invasion of the Body Snatchers (1956)',
-			'Metropolis (1927)',
-			'Terminator 2: Judgement Day (1991)',
-			'Forbidden Planet (1956)',
-			'Close Encounters of the Third Kind (1977)',
-			'Back to the Future (1985)',
-			'Brazil (1985)',
-			'Starship Troopers (1997)',
-			'Ex Machina (2014)',
-			'Wall-E (2008)',
-			'Inception (2010)',
-		];
-
-		const greatIdeas = [
-			'Travel to a country where you don’t speak the language',
-			'Go on a solo trip',
-			'Visit a “Dark Sky” site',
-			'Live abroad for a year',
-			'Take a pottery class',
-			'Plant a vegetable garden',
-			'Start a book club',
-			'Write a book',
-			'Teach a class',
-			'Become a mentor',
-			'Climb a mountain',
-			'Run a marathon',
-			'Start your own business',
-			'Plant a tree and watch it grow',
-			'Become a volunteer',
-			'Adopt a pet',
-			'Try skydiving',
-			'Take a hot air balloon ride',
-			'Learn to meditate',
-			'Join a dance class',
-			'Write a Letter to Your Future Self',
-			'Do Something, anything! You’ve never done',
-		];
-
 		const loadLists = (listsArray) => {
 			listsArray.forEach((list) => {
 				if (!Storage.masterList.findList(list)) {
@@ -854,12 +773,12 @@ export default class UI {
 			});
 		};
 
-		loadLists(lists);
-		loadTasks('TASKS', tasks);
-		loadTasks('Shopping', shopping);
-		loadTasks('Places to visit', placesToVisit);
-		loadTasks('Movies to watch', moviesToWatch);
-		loadTasks('Great ideas!', greatIdeas);
+		loadLists(ContentExample.lists);
+		loadTasks('TASKS', ContentExample.tasks);
+		loadTasks('Shopping', ContentExample.shopping);
+		loadTasks('Places to visit', ContentExample.placesToVisit);
+		loadTasks('Movies to watch', ContentExample.moviesToWatch);
+		loadTasks('Great ideas!', ContentExample.greatIdeas);
 
 		Storage.masterList.addStarInTask('TASKS', 'Finish The Odin Project');
 		Storage.masterList.addStarInTask('Movies to watch', 'The Matrix (1999)');
