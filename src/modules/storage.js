8c8522140ecccdf0a381af14a14337791ad5f579
Mariusz <casmar90@gmail.com>
2023-10-05 00:05:49 +0200

    Add new methods and change names

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -13,20 +13,31 @@ export default class Storage {
 		localStorage.clear();
 	}
 
-	static saveAll() {
+	static saveMasterList() {
 		localStorage.setItem('masterList', JSON.stringify(this.masterList.getLists()));
 	}
 
-	static recoverAll() {
+	static saveActiveList(activeList) {
+		localStorage.setItem('activeList', JSON.stringify(activeList));
+	}
+
+	static saveIsMuted(isMuted) {
+		localStorage.setItem('isMuted', JSON.stringify(isMuted));
+	}
+
+	static recoverMasterList() {
 		if (localStorage.getItem('masterList')) {
 			const recoveredData = JSON.parse(localStorage.getItem('masterList'));
 			this.resetMasterList();
-
 			this.masterList.lists = recoveredData.map((listData) => {
 				const list = new List(listData.name);
 				list.tasks = listData.tasks.map((taskData) => new Task(taskData.name, taskData.date, taskData.star, taskData.isDone));
 				return list;
 			});
 		}
+	}
+
+	static recoverActiveList() {
+		return JSON.parse(localStorage.getItem('activeList'));
 	}
 }
